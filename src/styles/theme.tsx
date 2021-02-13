9d1518e0e05a907b59f310813aa2d140d8f1dd17
Heath Ryu <32060873+heathryu@users.noreply.github.com>
2021-02-13 16:04:56 +0000

    Add ThemeProviders with colours and clean up unused files

---
+++ src/styles/theme.tsx
@@ -0,0 +1,27 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/styles';
+import { ThemeProvider as StyledComponentsThemeProvider } from 'styled-components';
+
+const colors = {
+  blastOffBronze: '#AD7D6C',
+  minionYellow: '#ECD444'
+};
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: colors.minionYellow
+    },
+    secondary: {
+      main: colors.blastOffBronze
+    }
+  }
+});
+
+export const ThemeProvider: React.FC = ({ children }) => (
+  <MuiThemeProvider theme={theme}>
+    <StyledComponentsThemeProvider theme={theme}>
+      {children}
+    </StyledComponentsThemeProvider>
+  </MuiThemeProvider>
+);
