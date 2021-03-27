150d752f58df310ef2bd1d44aa625a2d21023493
Heath Ryu <32060873+heathryu@users.noreply.github.com>
2021-03-27 16:24:11 +0000

    Update test to be relevant to current status of the app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,8 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const closeButton = screen.getByText(/close/i);
+  const submitButton = screen.getByText(/submit/i);
+  expect(closeButton).toBeInTheDocument();
+  expect(submitButton).toBeInTheDocument();
 });
